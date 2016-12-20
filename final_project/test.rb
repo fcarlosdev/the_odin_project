a28ec754340051e0d120f4a91ce9c854fd2027ae
fcarlosdev <fcarlosbr@gmail.com>
2016-12-19 23:24:16 -0300

    Removing unecessary file

--- final_project/test.rb
+++
@@ -1,8 +0,0 @@
-@white_pieces = {
-  :white_king  => "\u2654", :white_queen => "\u2655", :white_rook => "\u2656",
-  :white_bishop => "\u2657", :white_knight => "\u2658", :white_pawn => "\u2659"
-}
-@black_pieces = {
-  :black_king  => "\u265A", :black_queen => "\u265B", :black_rook => "\u265C",
-  :black_bishop => "\u265D", :black_knight => "\u265E", :black_pawn => "\u265F"
-}
