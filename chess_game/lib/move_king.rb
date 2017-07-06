0ddc21464a4285ba57ff56f7a9ed7bcd0cc0b124
fcarlosdev <fcarlosbr@gmail.com>
2017-07-05 21:46:18 -0300

    Implementing castling move.

--- chess_game/lib/move_king.rb
+++ chess_game/lib/move_king.rb
@@ -14,13 +14,9 @@ class MoveKing < MovePiece
       if valid_ordinary_move?(piece,to) || capture_move?(piece,to)
         board.move_piece(piece,to)
         return true
-      elsif piece.moves == 0
-        rook = board.value_from("g".concat(to[1]))
-        if (to > piece.position && !rook.nil? && rook.moves == 0 &&
-            rook.color == piece.color && rook.type == :rook)
-            board.move_piece(piece,to)
-            return true
-        end
+      elsif piece.moves == 0 && valid_castling_move?(piece,to)
+        board.move_piece(piece,to)
+        return true
       end
     end
     false
@@ -28,11 +24,38 @@ class MoveKing < MovePiece
   private
 
   def valid_ordinary_move?(piece,to)
-    empty_place?(to) && !opponent_to?(piece,to)
+    empty_place?(to) && !opponent_to?(piece,to) && move_by(piece.position,to) == 1
   end
 
   def capture_move?(piece,to)
     opponent_to?(piece,to)
+  end
+
+  def valid_castling_move?(piece,to)
+    has_castling_rook?(to,piece.position) && move_by(piece.position,to) == 2 &&
+    free_way?(piece.position,to)
+  end
+
+  def has_castling_rook?(to,from)
+    piece = value_at(rook_position(to,from))
+    piece != nil && piece.type == :rook && piece.moves == 0
+  end
+
+  def rook_position(to,from)
+    (from < to) ? "h".concat(to[1]) : "a".concat(to[1])
+  end
+
+  def free_way?(from,to)
+    [path(from,to)-[from]].flatten.all?{|position| value_at(position) == nil}
+  end
+
+  def path(from,to)
+    path = displacement(from,to).map{|i| [(from[0].ord + i).chr+from[1]]}.flatten
+    (!path.empty?) ? path : []
+  end
+
+  def displacement(from,to)
+    (from < to) ? (0..2) : (-2..0)
   end
 
   def empty_place?(to)
@@ -41,6 +64,14 @@ class MoveKing < MovePiece
 
   def opponent_to?(piece,at)
     !empty_place?(at) && board.value_from(at).color != piece.color
+  end
+
+  def value_at(position)
+    board.value_from(position)
+  end
+
+  def move_by(from,to)
+    calc_distance(from,to).abs
   end
 
 end
--- chess_game/spec/move_king_spec.rb
+++ chess_game/spec/move_king_spec.rb
@@ -6,9 +6,9 @@ describe "MoveKing" do
   let(:board) {Board.new}
   let(:move_king) {MoveKing.new(board)}
   let(:positions) {
-    {d1: "d1", d2: "d2", d4: "d4", d5: "d5", d8: "d8",
-     e1: "e1", e2: "e2", f1: "f1", f2: "f2", g1: "g1",
-     g3: "g3", g4: "g4", h1: "h1", h4: "h4"}}
+    {c1: "c1", d1: "d1", d2: "d2", d4: "d4", d5: "d5", d8: "d8",
+     e1: "e1", e2: "e2", f1: "f1", f2: "f2", g1: "g1", g3: "g3",
+     g4: "g4", h1: "h1", h4: "h4"}}
 
   let(:pieces) {
     {white_king: King.new(:white,positions[:e1]),
@@ -54,56 +54,11 @@ describe "MoveKing" do
 
     context "when is a valid castling move" do
       it "allows the king to make the castling move" do
-        board.clear_square(positions[:e2])
-        board.clear_square(positions[:f2])
-        expect(move_king.move(pieces[:white_king],positions[:f2])).to eq(true)
+        board.clear_square(positions[:f1])
+        board.clear_square(positions[:g1])
+        expect(move_king.move(pieces[:white_king],"g1")).to eq(true)
       end
     end
-
-    # context "when is a valid side move" do
-    #   it "moves the piece to the new position" do
-    #     board.clear_square(positions[:f1])
-    #     expect(move_king.move(pieces[:white_king],positions[:f1])).to eq(true)
-    #     expect(board.value_from(positions[:f1])).to eq(pieces[:white_king])
-    #   end
-    # end
-    #
-    # context "when is a valid diagonal move" do
-    #   it "moves the piece to the new position" do
-    #     board.clear_square(positions[:f2])
-    #     expect(move_king.move(pieces[:white_king],positions[:f2])).to eq(true)
-    #     expect(board.value_from(positions[:f2])).to eq(pieces[:white_king])
-    #   end
-    # end
-    #
-    # context "when is a valid capture move" do
-    #
-    #   context "when is a forward capture move" do
-    #     it "allows the piece to capture the opponent piece" do
-    #       board.fill_square(positions[:e2],pieces[:black_king])
-    #       expect(move_king.move(pieces[:white_king],positions[:e2])).to eq(true)
-    #       expect(board.value_from(positions[:e2])).to eq(pieces[:white_king])
-    #     end
-    #   end
-    #
-    #   context "when is a side capture move" do
-    #     it "allows the piece to capture the opponent piece" do
-    #       board.fill_square(positions[:f1],pieces[:black_king])
-    #       expect(move_king.move(pieces[:white_king],positions[:f1])).to eq(true)
-    #       expect(board.value_from(positions[:f1])).to eq(pieces[:white_king])
-    #     end
-    #   end
-    #
-    #   context "when is a diagonal capture move" do
-    #     it "allows the piece to capture the opponent piece" do
-    #       board.fill_square(positions[:f2],pieces[:black_king])
-    #       expect(move_king.move(pieces[:white_king],positions[:f2])).to eq(true)
-    #       expect(board.value_from(positions[:f2])).to eq(pieces[:white_king])
-    #     end
-    #   end
-    #
-    # end
-
 
   end
 
