af51a594851204b3e34c93237b4eb2ec0ca01d13
fcarlosdev <fcarlosbr@gmail.com>
2017-07-16 12:40:40 -0300

    Include the take_turn method.

--- chess_game/lib/player.rb
+++ chess_game/lib/player.rb
@@ -8,4 +8,18 @@ class Player
     @on_check = false
   end
 
+  def take_turn    
+    mount_message("from")
+    from = gets.chomp
+    mount_message("to")
+    to = gets.chomp
+    [from,to]
+  end
+
+  private
+
+  def mount_message(with_preposition)
+    print "Move piece " + with_preposition
+  end
+
 end
--- chess_game/spec/player_spec.rb
+++ chess_game/spec/player_spec.rb
@@ -6,10 +6,23 @@ describe "Player" do
     { player1: Player.new("Player1",:white),
       player2: Player.new("Player2",:black)}
   }
+  let(:moves) {["a2","a3"]}
+  let(:messages) {
+    ["Move piece from: ", "Move pice to: "]
+  }
 
   describe '#new' do
     it "creates a new instance of Player class" do
       expect(Player.new("Player1",:white)).to be_instance_of(Player)
+    end
+  end
+
+  describe '#take_turn' do
+
+    it "informs the moves that he wants to do" do
+      allow(players[:player1]).to receive(:print).and_return(messages[0], messages[1])
+      allow(players[:player1]).to receive_message_chain("gets.chomp").and_return(moves[0],moves[1])
+      expect(players[:player1].take_turn).to eq(moves)
     end
   end
 
