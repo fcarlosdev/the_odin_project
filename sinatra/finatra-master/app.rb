84c2eb0ccc3515e535eaeae1e64c466664083536
fcarlosdev <fcarlosbr@gmail.com>
2017-08-29 20:30:44 -0300

    Listing fishs.

--- sinatra/finatra-master/app.rb
+++ sinatra/finatra-master/app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
+require_relative "db/models"
 
 get '/' do
+  @fishes = Fish.all
   erb :index, layout: :main
 end
 
 get '/:fish_name' do
-  @fish = params[:fish_name]
+  @fish = Fish.find_by(name: params[:fish_name])
   erb :show, layout: :main
 end
--- sinatra/finatra-master/db/models.rb
+++ sinatra/finatra-master/db/models.rb
@@ -1,8 +1,5 @@
 require_relative "connection"
 
 class Fish < ActiveRecord::Base
-  validates :name, uniqueness: true
-  before_validation do
-    self.name.downcase!
-  end
+  
 end
