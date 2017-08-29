3497f34ca9052b92d9368dde995867e0d5a04d18
fcarlosdev <fcarlosbr@gmail.com>
2017-08-29 20:30:19 -0300

    Fixing code.

--- sinatra/finatra-master/db/schema.rb
+++ sinatra/finatra-master/db/schema.rb
@@ -2,8 +2,8 @@ require_relative "connection.rb"
 
 ActiveRecord::Schema.define do
   create_table :fish, force: :cascade do |t|
-    t.string :name,
-    t.string :image_url,
-    t.text   :description
-  end 
+    t.string  :name
+    t.string  :image_url
+    t.text    :description
+  end
 end
