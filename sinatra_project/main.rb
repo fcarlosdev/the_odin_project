d26a735f1b12bfe7ccb80f9cf78e3d92b8b4130c
fcarlosdev <fcarlosbr@gmail.com>
2017-09-03 16:40:49 -0300

    Including reference to caesar cipher application.

--- sinatra_project/main.rb
+++ sinatra_project/main.rb
@@ -1,9 +1,22 @@
+require './caesar_cipher_web/lib/caesar_cipher'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-# require './caesar_cipher_web/lib/caesar_cipher_web'
 # require './hangman_web/lib/'
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 
 get "/" do
-  # erb :index, :locals => {result: ""}
+  erb :index
+end
+
+get "/caesar_cipher" do
+  if !params["text"].nil? && !params["shift_by"].nil?
+    ciphered_text = cipher(params["text"], 1)
+  else
+    ciphered_text = ""
+  end
+  erb :index, :locals => {result: ciphered_text}
 end
