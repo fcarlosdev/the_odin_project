a5a85b0c45214edf34ce76efaf70cb2c45b7af7f
fcarlosdev <fcarlosbr@gmail.com>
2017-09-03 16:19:02 -0300

    Renaming project_sinatra to main.

---
+++ sinatra_project/main.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+# require './caesar_cipher_web/lib/caesar_cipher_web'
+# require './hangman_web/lib/'
+
+
+get "/" do
+  # erb :index, :locals => {result: ""}
+end
