24861fbee0c59abd5ddd60cd48f1a3c7c4931f3d
fcarlosdev <fcarlosbr@gmail.com>
2018-02-26 21:29:59 -0300

    Removing dead code.

--- private-events/app/models/invitation.rb
+++ private-events/app/models/invitation.rb
@@ -1,7 +1,4 @@
 class Invitation < ApplicationRecord
-
-  scope :accepted_invitations, -> { where(accepted: true)}
-
   belongs_to :attendee, class_name: "User"
   belongs_to :attended_event, class_name: "Event"
 end
