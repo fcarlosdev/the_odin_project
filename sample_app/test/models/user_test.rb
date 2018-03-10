7902790844818a63d789b46edac895de1d9598de
fcarlosdev <fcarlosbr@gmail.com>
2018-03-09 22:07:27 -0300

    "Add user following"

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -96,4 +96,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not francisco.following?(archer)
   end
 
+  test "feed should have the right posts" do
+     francisco = users(:francisco)
+     archer    = users(:archer)
+     lana      = users(:lana)
+     # Posts from followed user
+     lana.microposts.each do |post_following|
+       assert francisco.feed.include?(post_following)
+     end
+     # Posts from self
+     francisco.microposts.each do |post_self|
+       assert francisco.feed.include?(post_self)
+     end
+     # Posts from unfollowed user
+     archer.microposts.each do |post_unfollowed|
+       assert_not francisco.feed.include?(post_unfollowed)
+     end
+   end
+
 end
