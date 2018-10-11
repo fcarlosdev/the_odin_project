967f0d390fa396cfa3353f180e4cb650b2e13499
fcarlosdev <fcarlosbr@gmail.com>
2018-10-10 21:11:21 -0300

    Firt commint.

---
+++ js_tic_tac_toe/js/script.js
@@ -0,0 +1,10 @@
+let gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach(function(item){
+
+  item.addEventListener('click',function(e) {
+    e.preventDefault();
+    this.textContent = (this.textContent == "X") ? "0" : "X";
+  });
+
+})
