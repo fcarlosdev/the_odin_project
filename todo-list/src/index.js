d6ecb80ee6266cced03ca24ce0702d5224eb619e
fcarlosdev <fcarlosbr@gmail.com>
2018-11-12 13:23:36 -0300

    Writing to the index body.

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -4,4 +4,13 @@ import {todoFactory} from './modules/todoFactory.js'
 
 const todoOne = todoFactory("Todo 1", "Teste of todo", "12/11/2018", "media");
 
-console.log(todoOne.getTitle());
+var div = document.createElement('div');
+
+div.innerHTML += "Todo list <br>";
+div.innerHTML += "<hr>";
+div.innerHTML += "Title: " + todoOne.getTitle() +"<br>";
+div.innerHTML += "Description: " + todoOne.getDescription() +"<br>";
+div.innerHTML += "DueDate: " + todoOne.getDueDate() +"<br>";
+div.innerHTML += "Priority: " + todoOne.getPriority();
+
+document.querySelector("body").appendChild(div);
