6765d8ae1d3e37b97367499faa5888fbae40aa20
fcarlosdev <fcarlosbr@gmail.com>
2018-11-26 21:50:40 -0300

    Improve the layout.

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,40 +1,46 @@
 import './css/main.css';
-// import * Todo from './modules/todoFactory.js';
 import {todoFactory} from './modules/todoFactory.js';
 import {projectFactory} from './modules/project.js';
 
 const todoOne = todoFactory("Todo 1", "Teste of todo", "12/11/2018", "medium");
 const todoTwo = todoFactory("Todo 2", "JavaScript OO", "15/11/2018", "medium");
 
-const myTodoProject = projectFactory("Learning JavasScript");
+const myTodoProject = projectFactory("Todo List");
 myTodoProject.addTodoItem(todoOne);
 myTodoProject.addTodoItem(todoTwo);
 
-var div = document.createElement('div');
-listTodoItems();
-document.querySelector("body").appendChild(div);
+let projects = document.querySelector('.projects-container');
+let project = document.createElement('div');
+project.setAttribute('id','1');
+project.classList.add('project');
+project.append(myTodoProject.getName());
+projects.appendChild(project);
 
-var messageDiv = document.createElement('div');
-
-if (myTodoProject.removeTodoItem(todoOne)) {
-  messageDiv.innerHTML += "Todo: " + todoOne.getTitle() + " removed <br>";
-} else {
-  messageDiv.innerHTML += "Error to remove " + todoOne.getTitle() + "<br>";
-}
-div.innerHTML = "";
-messageDiv.innerHTML += "<hr>";
-document.querySelector("body").appendChild(messageDiv);
-listTodoItems();
-
-
-function listTodoItems() {
-  div.innerHTML += "Project: " + myTodoProject.getName() + "<br>";
-  div.innerHTML += "<hr>";
-  myTodoProject.getTodoList().forEach(function(item) {
-    div.innerHTML += "Title: " + item.getTitle() + "<br>";
-    div.innerHTML += "Description: " + todoOne.getDescription() +"<br>";
-    div.innerHTML += "DueDate: " + todoOne.getDueDate() +"<br>";
-    div.innerHTML += "Priority: " + todoOne.getPriority() +"<br>";
-    div.innerHTML += "<hr>";
-  });
-}
+// var div = document.createElement('div');
+// listTodoItems();
+// document.querySelector("body").appendChild(div);
+//
+// var messageDiv = document.createElement('div');
+//
+// if (myTodoProject.removeTodoItem(todoOne)) {
+//   messageDiv.innerHTML += "Todo: " + todoOne.getTitle() + " removed <br>";
+// } else {
+//   messageDiv.innerHTML += "Error to remove " + todoOne.getTitle() + "<br>";
+// }
+// div.innerHTML = "";
+// messageDiv.innerHTML += "<hr>";
+// document.querySelector("body").appendChild(messageDiv);
+// listTodoItems();
+//
+//
+// function listTodoItems() {
+//   div.innerHTML += "Project: " + myTodoProject.getName() + "<br>";
+//   div.innerHTML += "<hr>";
+//   myTodoProject.getTodoList().forEach(function(item) {
+//     div.innerHTML += "Title: " + item.getTitle() + "<br>";
+//     div.innerHTML += "Description: " + todoOne.getDescription() +"<br>";
+//     div.innerHTML += "DueDate: " + todoOne.getDueDate() +"<br>";
+//     div.innerHTML += "Priority: " + todoOne.getPriority() +"<br>";
+//     div.innerHTML += "<hr>";
+//   });
+// }
