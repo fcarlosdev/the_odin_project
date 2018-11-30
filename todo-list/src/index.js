5ae82f11d7ddfd634cf1bce9c689d8d52f1a0aeb
fcarlosdev <fcarlosbr@gmail.com>
2018-11-30 10:40:23 -0300

    Implementing creation of tasks behavior.

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -12,6 +12,9 @@ let addTodo     = appView.querySelector('#add-todo');
 let todoTxtName = appView.querySelector('#txt-n-list');
 let todos       = appView.querySelector('.projects-container');
 let todoTasks   = appView.querySelector('#todo-tasks');
+let todoTitle   = appView.querySelector('#todo-title');
+let tdName      = appView.querySelector('#title-name');
+let tdNewTask   = appView.querySelector('#new-task');
 
 let controller = null;
 
@@ -25,56 +28,9 @@ todoTxtName.addEventListener('keydown', function(event) {
   }
 });
 
-// let taskData = {
-//
-//   taskone : {
-//     title: "Learn fundamentals",
-//     description: "Knowing the data types, the math operator, etc",
-//     dueDate: "30/11/2018",
-//     priority: "medium"
-//   },
-//   tasktwo: {
-//     title: "Loop structures",
-//     description: "for-loop, forEach loop, while, etc",
-//     dueDate: "01/12/2018",
-//     priority: "medium"
-//   }
-// }
-//
 // todo.addTask(controller.newTask(taskData['taskone']));
 // todo.addTask(controller.newTask(taskData['tasktwo']));
 //
-// appView.querySelector('.projects-container').append(todoDiv);
-//
-// todoDiv.addEventListener('click', function(e) {
-//
-//   todo.getTasks().forEach(function(task) {
-//     let taskItem = document.createElement('li');
-//
-//     let checkCircle = document.createElement('img');
-//     checkCircle.setAttribute('status','not-done');
-//     checkCircle.src = 'images/circle-regular.svg';
-//     checkCircle.classList.add('img');
-//     checkCircle.addEventListener('click', function(e) {
-//         e.preventDefault();
-//         if (checkCircle.getAttribute('status') === 'not-done') {
-//           checkCircle.src = 'images/circle-solid.svg'
-//           checkCircle.setAttribute('status','done');
-//         } else {
-//           checkCircle.src = 'images/circle-regular.svg';
-//           checkCircle.setAttribute('status','not-done');
-//         }
-//     });
-//
-//     taskItem.appendChild(checkCircle);
-//     taskItem.append(task.getTitle());
-//
-//     taskList.appendChild(taskItem);
-//
-//   });
-//   todoTasks.appendChild(taskList);
-//
-// })
 
 const newTodo = (todoName) => {
   controller = todoController( { todoFactory: Factory } );
@@ -88,11 +44,79 @@ const newTodo = (todoName) => {
   todoTxtName.value = '';
 
   todoDiv.addEventListener('click',function() {
-      let contentTodoTitle           = document.querySelector('#todo-title');
-      contentTodoTitle.textContent   = todo.getName();
-      contentTodoTitle.style.display = 'block';
 
-      if (todo.hasAnyTask()) {
+      let lkNewTask = document.createElement('a');
+      lkNewTask.setAttribute('ur','#');
+      lkNewTask.style.cursor = 'pointer';
+      lkNewTask.textContent = "+";
+
+      lkNewTask.addEventListener('click',function() {
+        let newTaskDiv                   = document.createElement('div');
+        newTaskDiv.style.width           = "100%";
+        newTaskDiv.style.display         = "flex";
+        newTaskDiv.justifyContent        = "space-between";
+
+
+        let newTaskDiv1                  = document.createElement('div');
+        newTaskDiv1.style.width          = "100%";
+        let taskTitleInput               = document.createElement('input');
+        taskTitleInput.placeholder       = "Task Title";
+        taskTitleInput.style.width       = "100%";
+        // taskTitleInput.style.marginRight = "5px";
+        taskTitleInput.classList.add("input-data");
+        newTaskDiv1.appendChild(taskTitleInput);
+
+        let newTaskDiv2                   = document.createElement('div');
+        newTaskDiv2.style.width          = "100%";
+        let taskDescTextArea               = document.createElement('input');
+        taskDescTextArea.placeholder       = "Task description";
+        taskDescTextArea.style.width       = "75%";
+        // taskDescTextArea.style.marginRight = "5px";
+        taskDescTextArea.classList.add("input-data");
+        newTaskDiv2.appendChild(taskDescTextArea);
+        //
+        // let newTaskDiv3                   = document.createElement('div');
+        // let taskDueDateInput               = document.createElement('input');
+        // taskDueDateInput.placeholder       = "Due Date";
+        // taskDueDateInput.style.width       = "auto";
+        // // taskDueDateInput.style.marginRight = "5px";
+        // taskDueDateInput.classList.add("input-data");
+        //
+        // let taskPrioritSelect               = document.createElement('select');
+        // taskPrioritSelect.style.width       = "auto";
+        // taskPrioritSelect.classList.add("input-data");
+
+        // let optionDefault = document.createElement("option");
+        // let optionLow = document.createElement("option");
+        // let optionMedium = document.createElement("option");
+        // let optionHight = document.createElement("option");
+
+        // optionDefault.text = "Priority";
+        // optionLow.text     = "Low";
+        // optionMedium.text  = "Medium";
+        // optionHight.text   = "Hight";
+        //
+        // taskPrioritSelect.add(optionDefault);
+        // taskPrioritSelect.add(optionLow);
+        // taskPrioritSelect.add(optionMedium);
+        // taskPrioritSelect.add(optionHight);
+
+        newTaskDiv.appendChild(newTaskDiv1);
+        newTaskDiv.appendChild(newTaskDiv2);
+        // newTaskDiv.appendChild(taskTitleInput);
+        // newTaskDiv.appendChild(taskDescTextArea);
+        // newTaskDiv.appendChild(taskDueDateInput);
+        // newTaskDiv.appendChild(taskPrioritSelect);
+        todoTasks.appendChild(newTaskDiv);
+      })
+
+    tdName.textContent = todo.getName();
+    tdNewTask.appendChild(lkNewTask);
+    todoTitle.appendChild(tdName);
+    todoTitle.appendChild(tdNewTask);
+    todoTitle.style.display ='flex';
+
+    if (todo.hasAnyTask()) {
         let taskList             = document.createElement('ul');
         taskList.style.listStyle = 'none';
         taskList.style.margin    = '0';
@@ -101,10 +125,10 @@ const newTodo = (todoName) => {
 
         todo.getTasks().forEach(function(task) {
 
-            let taskItem = document.createElement('li');
+            let taskItem    = document.createElement('li');
             let checkCircle = document.createElement('img');
-            checkCircle.setAttribute('status','not-done');
             checkCircle.src = 'images/circle-regular.svg';
+            checkCircle.setAttribute('status','not-done');
             checkCircle.classList.add('img');
 
             checkCircle.addEventListener('click', function() {
