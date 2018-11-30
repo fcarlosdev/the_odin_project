56d0f7fe18e6b7847169b510f523b6498e6b48c3
fcarlosdev <fcarlosbr@gmail.com>
2018-11-30 14:54:33 -0300

    Implementing task add form.

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -28,9 +28,6 @@ todoTxtName.addEventListener('keydown', function(event) {
   }
 });
 
-// todo.addTask(controller.newTask(taskData['taskone']));
-// todo.addTask(controller.newTask(taskData['tasktwo']));
-//
 
 const newTodo = (todoName) => {
   controller = todoController( { todoFactory: Factory } );
@@ -55,59 +52,94 @@ const newTodo = (todoName) => {
         newTaskDiv.style.width           = "100%";
         newTaskDiv.style.display         = "flex";
         newTaskDiv.justifyContent        = "space-between";
+        newTaskDiv.style.paddingTop      = "10px";
 
 
         let newTaskDiv1                  = document.createElement('div');
-        newTaskDiv1.style.width          = "100%";
+        newTaskDiv1.style.width          = "20%";
+        newTaskDiv1.style.marginRight    = "20px";
+
         let taskTitleInput               = document.createElement('input');
         taskTitleInput.placeholder       = "Task Title";
         taskTitleInput.style.width       = "100%";
-        // taskTitleInput.style.marginRight = "5px";
         taskTitleInput.classList.add("input-data");
         newTaskDiv1.appendChild(taskTitleInput);
 
-        let newTaskDiv2                   = document.createElement('div');
-        newTaskDiv2.style.width          = "100%";
+        let newTaskDiv2                    = document.createElement('div');
+        newTaskDiv2.style.width            = "45%";
+        newTaskDiv2.style.marginRight      = "20px";
+
         let taskDescTextArea               = document.createElement('input');
         taskDescTextArea.placeholder       = "Task description";
-        taskDescTextArea.style.width       = "75%";
-        // taskDescTextArea.style.marginRight = "5px";
+        taskDescTextArea.style.width       = "100%";
         taskDescTextArea.classList.add("input-data");
         newTaskDiv2.appendChild(taskDescTextArea);
-        //
-        // let newTaskDiv3                   = document.createElement('div');
-        // let taskDueDateInput               = document.createElement('input');
-        // taskDueDateInput.placeholder       = "Due Date";
-        // taskDueDateInput.style.width       = "auto";
-        // // taskDueDateInput.style.marginRight = "5px";
-        // taskDueDateInput.classList.add("input-data");
-        //
-        // let taskPrioritSelect               = document.createElement('select');
-        // taskPrioritSelect.style.width       = "auto";
-        // taskPrioritSelect.classList.add("input-data");
 
-        // let optionDefault = document.createElement("option");
-        // let optionLow = document.createElement("option");
-        // let optionMedium = document.createElement("option");
-        // let optionHight = document.createElement("option");
+        let newTaskDiv3                    = document.createElement('div');
+        newTaskDiv3.style.width            = "10%";
+        newTaskDiv3.style.marginRight      = "20px";
 
-        // optionDefault.text = "Priority";
-        // optionLow.text     = "Low";
-        // optionMedium.text  = "Medium";
-        // optionHight.text   = "Hight";
-        //
-        // taskPrioritSelect.add(optionDefault);
-        // taskPrioritSelect.add(optionLow);
-        // taskPrioritSelect.add(optionMedium);
-        // taskPrioritSelect.add(optionHight);
+        let taskDueDateInput               = document.createElement('input');
+        taskDueDateInput.placeholder       = "Due Date";
+        taskDueDateInput.style.width       = "100%";
+        taskDueDateInput.classList.add("input-data");
+        newTaskDiv3.appendChild(taskDueDateInput);
+
+        let newTaskDiv4                     = document.createElement('div');
+        newTaskDiv4.style.width             = "6%";
+        newTaskDiv4.style.marginRight      = "10px";
+
+        let taskPrioritSelect               = document.createElement('select');
+        taskPrioritSelect.style.width       = "100%";
+        taskPrioritSelect.classList.add("input-data");
+        newTaskDiv4.appendChild(taskPrioritSelect);
+
+        let optionDefault = document.createElement("option");
+        let optionLow = document.createElement("option");
+        let optionMedium = document.createElement("option");
+        let optionHight = document.createElement("option");
+
+        optionDefault.text = "Priority";
+        optionLow.text     = "Low";
+        optionMedium.text  = "Medium";
+        optionHight.text   = "Hight";
+
+        taskPrioritSelect.add(optionDefault);
+        taskPrioritSelect.add(optionLow);
+        taskPrioritSelect.add(optionMedium);
+        taskPrioritSelect.add(optionHight);
 
         newTaskDiv.appendChild(newTaskDiv1);
         newTaskDiv.appendChild(newTaskDiv2);
-        // newTaskDiv.appendChild(taskTitleInput);
-        // newTaskDiv.appendChild(taskDescTextArea);
-        // newTaskDiv.appendChild(taskDueDateInput);
-        // newTaskDiv.appendChild(taskPrioritSelect);
+        newTaskDiv.appendChild(newTaskDiv3);
+        newTaskDiv.appendChild(newTaskDiv4);
+
+        let btnSaveDiv = document.createElement('div');
+        btnSaveDiv.style.width = "4%";
+
+        let btnCancelDiv = document.createElement('div');
+        btnCancelDiv.style.width = "4%";        
+
+        let btnSave = document.createElement('button');
+        btnSave.style.width = "100%";
+        btnSave.style.height = "100%";
+        btnSave.textContent = "Save";
+
+        let btnCancel = document.createElement('button');
+        btnCancel.style.width = "100%";
+        btnCancel.style.height = "100%";
+        btnCancel.textContent = "Cancel";
+
+
+
+        btnSaveDiv.appendChild(btnSave);
+        btnCancelDiv.appendChild(btnCancel);
+
+        newTaskDiv.appendChild(btnSaveDiv);
+        newTaskDiv.appendChild(btnCancelDiv);
+
         todoTasks.appendChild(newTaskDiv);
+
       })
 
     tdName.textContent = todo.getName();
