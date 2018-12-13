d5da441654437e8b6dc3bbd58ad5d966ae2ee95c
fcarlosdev <fcarlosbr@gmail.com>
2018-12-13 07:20:56 -0300

    Reverting commits

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,5 +1,4 @@
 import "./util/imports.js";
-import "./util/window-events.js";
 import {TodoController} from "./controllers/todoController.js";
 // import {TaskController} from "./controllers/taskController.js";
 import {App} from "./elements/app-todo.js";
@@ -37,3 +36,37 @@ const newTask = (todo) => {
 const clearTextField = (textField) => {
   textField.value = "";
 }
+
+
+
+// let detailsStatus = "closed";
+// const todoView = document.querySelector(".container");
+//
+//
+//
+// const btDetails = todoView.querySelector(".bt-details");
+//
+// btDetails.addEventListener('click',function() {
+//   let taskTitle = todoView.querySelector(".task-title");
+//   let taskDetails = todoView.querySelector(".task-details");
+//   if (detailsStatus === "closed") {
+//     detailsStatus = "opened";
+//     taskDetails.style.display = "flex";
+//     taskDetails.style.flexDirection = "column";
+//     taskTitle.querySelector("a").textContent = "Hide details";
+//   } else {
+//     detailsStatus = "closed";
+//     taskDetails.style.display = "none";
+//     taskTitle.querySelector("a").textContent = "Show details";
+//   }
+// });
+//
+// todoView.querySelector("#tsk-done").addEventListener('click',function(c) {
+//   if (this.checked) {
+//     todoView.querySelector(".task-done").style.color = "#1b7bdf";
+//     todoView.querySelector("#tsk-one").querySelector('h2').style.textDecoration="line-through";
+//   } else {
+//     todoView.querySelector(".task-done").style.color = "#000";
+//     todoView.querySelector("#tsk-one").querySelector('h2').style.textDecoration="none";
+//   }
+// })
