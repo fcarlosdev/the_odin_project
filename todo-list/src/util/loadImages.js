a3198143c75ac2b861e3101213d5dc780ee345d5
fcarlosdev <fcarlosbr@gmail.com>
2018-12-16 20:39:30 -0300

    Removing project don't used.

--- todo-list/src/util/loadImages.js
+++
@@ -1,9 +0,0 @@
-const importImages = (from) => {
-  let req = require.context("../images", false, /\.(png|svg|jpg|gif|jpeg)$/);
-  req.keys().forEach(function(key){
-      req(key);
-  });
-  return req;
-}
-
-export { importImages }
