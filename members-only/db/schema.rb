c20ae933646897a5bd31d9ebb2cb478ee29cca6f
fcarlosdev <fcarlosbr@gmail.com>
2019-03-17 23:55:22 -0300

    Update rails version

--- members-only/db/schema.rb
+++ members-only/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207235547) do
+ActiveRecord::Schema.define(version: 2018_02_07_235547) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
